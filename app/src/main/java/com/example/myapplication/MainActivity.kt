9045ddc8f8e4466bec7477386b2f7123e68f2218
vaibhav <codekul.vaibhav@gmail.com>
2021-04-01 12:33:41 +0530

    First commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    var txtValue: TextView? = null
+    var btnIncreament: Button? = null
+    var number: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        txtValue = findViewById(R.id.btnIncreament)
+        btnIncreament = findViewById(R.id.btnIncreament)
+
+        btnIncreament?.setOnClickListener {
+
+            number++
+            txtValue?.setText(number.toString())
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "On Start", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "On Resume", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "On Pause", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "On Stop", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "On Destroy", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Toast.makeText(this, "On Restart", Toast.LENGTH_LONG).show()
+    }
+
+
+}
